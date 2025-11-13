5759e9be1e6add60d9d0ed4db7958350846076ff
Gaurav Verma <122605852+GrvExplorer@users.noreply.github.com>
2025-11-13 23:40:11 +0530

    file location fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from "./app.tsx";
+import App from "./App.tsx";
 import "./app.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
